dfdacae22d69444464bb150afd11f8b03675e7f9
Silvere <silvere.heraudeau@gmail.com>
2021-07-25 15:45:14 -0600

    Updated generation script to handle all images.

---
+++ pineapples-generation/README.md
@@ -0,0 +1,60 @@
+# Pineapples Generation
+
+How to generate pineapple images:
+
+## Installation
+
+Before being able to run this program, you'll need a few programs:
+
+- Python 3: You can download and install python 3 [here][1].
+- Libjpeg: This dependency is necessary for Pillow to function, you can install it with the following command in the command line.
+
+```
+brew install libjpeg
+```
+
+- Zlib: This other dependency is necessary for Pillow to run properly, you can find it [here][2].
+- Pillow: You can follow [this tutorial][4] to install the Pillow library.
+
+[1]: https://www.python.org/downloads/ "Python download page"
+[2]: https://www.zlib.net/ "Zlib website"
+[4]: https://pillow.readthedocs.io/en/stable/installation.html#basic-installation "Commands to install the Pillow library"
+
+## Directory Structure
+
+The script is programmed to function with the following directory structure:
+
+```
+- _assets
+	- ac
+	- bg
+	- cr
+	- ey
+	- fw
+	- mo
+	- sk
+	- output
+```
+
+## Filename conventions
+
+The files should be name respecting the following conventions:
+
+`ac1.png`, `ac2.png`, etc...
+
+Those files should be placed in their respective directories.
+In addition, an empty transparent image named `none.png` should be placed in `_assets`
+
+## Usage
+
+To run the script, simply type the following in the command line.
+```
+python3 generate.py <number>
+```
+
+By default, the script will produce 50 images, to produce more, type the number of desired images instead of `<number>`
+
+## Output
+
+The script will produce a number of images named `0.jpg`, `1.jpg`, etc... in the `_assets/output` directory.
+It will also produce a file named `metadata.json` in the directory `_assets` containing a summary of all produced images.
--- pineapples-generation/generate.py
+++ pineapples-generation/generate.py
@@ -1,86 +1,402 @@
 from PIL import Image, ImageOps
+import sys
 import random
 import json
 
 # Declare traits
-bg = ["Grey", "Hot Pink", "Red"]
-bg_weight = [45, 24, 31]
-
-sk = ["Olive", "Orange", "Pink"]
-sk_weight = [18, 76, 6]
-
-ey = ["Eyepatch", "Heart", "X"]
-ey_weight = [1, 84, 15]
-
-cr = ["Blue", "Grey", "Mixed"]
-cr_weight = [35, 12, 53]
-
-mo = ["Growl", "Straight", "Tongue Out"]
-mo_weight = [63, 30, 7]
-
-fw = ["Slides", "Socks", "None"]
-fw_weight = [32, 19, 49]
-
-ac = ["Balloon", "Kite", "None"]
-ac_weight = [26, 11, 63]
-
+bg = [
+	"Light Yellow",
+	"Grey",
+	"Orange",
+	"Light Green",
+	"Hot Pink",
+	"Purple",
+	"Red",
+	"Baby Blue",
+	"Gold",
+	"Rainbow"
+]
+bg_weight = [
+	23.2,
+	20,
+	17,
+	13,
+	11,
+	8,
+	5,
+	2,
+	0.5,
+	0.3
+]
 bg_files = {
-	"Grey"				: "Bg1",
-	"Hot Pink"		: "Bg2",
-	"Red"					: "Bg3"
+	"Light Yellow": "bg1",
+	"Grey": "bg2",
+	"Orange": "bg3",
+	"Light Green": "bg4",
+	"Hot Pink": "bg5",
+	"Purple": "bg6",
+	"Red": "bg7",
+	"Baby Blue": "bg8",
+	"Gold": "bg9",
+	"Rainbow": "bg10"
 }
 
+sk = [
+	"Orange",
+	"Peach",
+	"Light Yellow",
+	"Turquoise",
+	"Hot Pink",
+	"Mint Blue",
+	"Plum",
+	"Royal Blue",
+	"Lime Green",
+	"Olive Green",
+	"Pastel Pink",
+	"Lilac",
+	"Mixed",
+	"Psychedelic",
+	"Gold"
+]
+sk_weight = [
+	13.4,
+	13,
+	12.5,
+	12,
+	11.3,
+	9.1,
+	8,
+	7.1,
+	5.2,
+	3.9,
+	2.1,
+	1,
+	0.7,
+	0.4,
+	0.3
+]
 sk_files = {
-	"Olive"				: "Sk1",
-	"Orange"			: "Sk2",
-	"Pink"				: "Sk3"
+	"Orange": "sk1",
+	"Peach": "sk2",
+	"Light Yellow": "sk3",
+	"Turquoise": "sk4",
+	"Hot Pink": "sk5",
+	"Mint Blue": "sk6",
+	"Plum": "sk7",
+	"Royal Blue": "sk8",
+	"Lime Green": "sk9",
+	"Olive Green": "sk10",
+	"Pastel Pink": "sk11",
+	"Lilac": "sk12",
+	"Mixed": "sk13",
+	"Psychedelic": "sk14",
+	"Gold": "sk15"
 }
 
+ey = [
+	"Default",
+	"Closed",
+	"Angry",
+	"Sad",
+	"Nerd Glasses",
+	"3D Glasses",
+	"Curious",
+	"Blindfold",
+	"Stars",
+	"Lazy",
+	"X",
+	"Hearts",
+	"High",
+	"Rolling",
+	"Shades",
+	"Eye Patch",
+	"Dollar Signs",
+	"Googly",
+	"Hypnotized" 
+]
+ey_weight = [
+	15.6,
+	14,
+	11.8,
+	9,
+	8.1,
+	7,
+	6.1,
+	5.4,
+	4.6,
+	3.7,
+	3.4,
+	3.1,
+	2.9,
+	2,
+	1.2,
+	1,
+	0.6,
+	0.4,
+	0.1 
+]
 ey_files = {
-	"Eyepatch"		: "Ey1",
-	"Heart"				: "Ey2",
-	"X"						: "Ey3"
+	"Default": "ey1",
+	"Closed": "ey2",
+	"Angry": "ey3",
+	"Sad": "ey4",
+	"Nerd Glasses": "ey5",
+	"3D Glasses": "ey6",
+	"Curious": "ey7",
+	"Blindfold": "ey8",
+	"Stars": "ey9",
+	"Lazy": "ey10",
+	"X": "ey11",
+	"Hearts": "ey12",
+	"High": "ey13",
+	"Rolling": "ey14",
+	"Shades": "ey15",
+	"Eye Patch": "ey16",
+	"Dollar Signs": "ey17",
+	"Googly": "ey18",
+	"Hypnotized": "ey19" 
 }
 
+cr = [
+	"Green",
+	"Mustard",
+	"Orange",
+	"Grey",
+	"Blue",
+	"Purple",
+	"Pink",
+	"Silver",
+	"Gold",
+	"Mixed",
+	"Broken Gold",
+	"Broken Silver"
+]
+cr_weight = [
+	24.2,
+	21,
+	15,
+	13,
+	11,
+	5,
+	4.7,
+	2.3,
+	2,
+	1,
+	0.4,
+	0.4
+]
 cr_files = {
-	"Blue"				: "Cr1",
-	"Grey"				: "Cr2",
-	"Mixed"				: "Cr3"
+	"Green": "cr1",
+	"Mustard": "cr2",
+	"Orange": "cr3",
+	"Grey": "cr4",
+	"Blue": "cr5",
+	"Purple": "cr6",
+	"Pink": "cr7",
+	"Silver": "cr8",
+	"Gold": "cr9",
+	"Mixed": "cr10",
+	"Broken Gold": "cr11",
+	"Broken Silver": "cr12"
 }
 
+mo = [
+	"Happy",
+	"Sad",
+	"Cheeky",
+	"Angry",
+	"Kissy Heart",
+	"Smirk",
+	"Party Horn",
+	"Open",
+	"Cigar",
+	"Bubble Gum",
+	"Vampire",
+	"Toothpick",
+	"Tongue Out",
+	"Growl",
+	"Gold Teeth",
+	"Whistle",
+	"Blunt",
+	"Bites Lip",
+	"Rainbow Teeth",
+	"Drooling",
+	"Yikes",
+	"Censored",
+	"Pacifier",
+	"Golden Vampire Tooth" 
+]
+mo_weight = [
+	7,
+	6.6,
+	6.5,
+	6.4,
+	6.4,
+	6.3,
+	6.3,
+	6.1,
+	5.7,
+	5.5,
+	5.1,
+	4.9,
+	4.4,
+	4.1,
+	3.8,
+	3.3,
+	3,
+	2.5,
+	2.1,
+	1.7,
+	1.2,
+	0.7,
+	0.3,
+	0.1
+]
 mo_files = {
-	"Growl"				: "Mo1",
-	"Straight"		: "Mo2",
-	"Tongue Out"	: "Mo3"
+	"Happy": "mo1",
+	"Sad": "mo2",
+	"Cheeky": "mo3",
+	"Angry": "mo4",
+	"Kissy Heart": "mo5",
+	"Smirk": "mo6",
+	"Party Horn": "mo7",
+	"Open": "mo8",
+	"Cigar": "mo9",
+	"Bubble Gum": "mo10",
+	"Vampire": "mo11",
+	"Toothpick": "mo12",
+	"Tongue Out": "mo13",
+	"Growl": "mo14",
+	"Gold Teeth": "mo15",
+	"Whistle": "mo16",
+	"Blunt": "mo17",
+	"Bites Lip": "mo18",
+	"Rainbow Teeth": "mo19",
+	"Drooling": "mo20",
+	"Yikes": "mo21",
+	"Censored": "mo22",
+	"Pacifier": "mo23",
+	"Golden Vampire Tooth": "mo24" 
 }
 
+fw = [
+	"None",
+	"Plain Socks",
+	"Sneakers",
+	"Sandals",
+	"Slides",
+	"Gumboots",
+	"Crocs",
+	"Polka Dotted Socks",
+	"Timberlands",
+	"Striped Socks"
+]
+fw_weight = [
+	31.6,
+	21,
+	18,
+	12,
+	8,
+	4,
+	3,
+	1.2,
+	0.8,
+	0.4
+]
 fw_files = {
-	"Slides"			: "Fw1",
-	"Socks"				: "Fw2"
+	"Plain Socks": "fw1",
+	"Sneakers": "fw2",
+	"Sandals": "fw3",
+	"Slides": "fw4",
+	"Gumboots": "fw5",
+	"Crocs": "fw6",
+	"Polka Dotted Socks": "fw7",
+	"Timberlands": "fw8",
+	"Striped Socks": "fw9"
 }
 
+ac = [
+	"None",
+	"Balloon",
+	"Lollipop",
+	"Foam Finger",
+	"Devil Horns",
+	"Popsicle",
+	"Headband",
+	"Wristband",
+	"Drink",
+	"Baseball",
+	"Bandana",
+	"Kite",
+	"Pizza Slice",
+	"Tambourine",
+	"Bow-tie",
+	"Binoculars",
+	"Surfboard",
+	"Magnifying Glass",
+	"Underwear" 
+]
+ac_weight = [
+	5,
+	12.1,
+	11.6,
+	10.7,
+	9.4,
+	8.6,
+	8,
+	7.4,
+	6,
+	5.2,
+	4,
+	3.1,
+	2.7,
+	2,
+	1.6,
+	1,
+	0.9,
+	0.5,
+	0.2 
+]
 ac_files = {
-	"Balloon"			: "Ac1",
-	"Kite"				: "Ac2"
+	"Balloon": "ac1",
+	"Lollipop": "ac2",
+	"Foam Finger": "ac3",
+	"Devil Horns": "ac4",
+	"Popsicle": "ac5",
+	"Headband": "ac6",
+	"Wristband": "ac7",
+	"Drink": "ac8",
+	"Baseball": "ac9",
+	"Bandana": "ac10",
+	"Kite": "ac11",
+	"Pizza Slice": "ac12",
+	"Tambourine": "ac13",
+	"Bow-tie": "ac14",
+	"Binoculars": "ac15",
+	"Surfboard": "ac16",
+	"Magnifying Glass": "ac17",
+	"Underwear": "ac18" 
 }
 
 # Trait Generation
 
-TOTAL = 300
+if len(sys.argv) != 1: 
+	TOTAL = int(sys.argv[1])
+else:
+	TOTAL = 50
 
 traits = []
 
 def createSet():
 	trait = {}
 
-	trait["TokenID"] 				= ""
-	trait["Background"] 		= random.choices(bg, bg_weight)[0]
-	trait["Skin"] 					= random.choices(sk, sk_weight)[0]
-	trait["Mouth"] 					= random.choices(mo, mo_weight)[0]
-	trait["Eyes"] 					= random.choices(ey, ey_weight)[0]
-	trait["Crown"] 					= random.choices(cr, cr_weight)[0]
-	trait["Footwear"] 			= random.choices(fw, fw_weight)[0]
-	trait["Accessories"] 		= random.choices(ac, ac_weight)[0]
+	trait["Background"] = random.choices(bg, bg_weight)[0]
+	trait["skin"] = random.choices(sk, sk_weight)[0]
+	trait["Mouth"] = random.choices(mo, mo_weight)[0]
+	trait["Eyes"] = random.choices(ey, ey_weight)[0]
+	trait["Crown"] = random.choices(cr, cr_weight)[0]
+	trait["Footwear"] = random.choices(fw, fw_weight)[0]
+	trait["Accessories"] = random.choices(ac, ac_weight)[0]
 
 	if trait in traits:
 		return createSet()
@@ -98,6 +414,13 @@ def allUnique(x):
 
 print(allUnique(traits))
 
+# Name pineapples according to their number
+i = 0
+for item in traits:
+	item["TokenID"] = i
+	item["Name"] = "Pineapple #" + str(i)
+	i = i + 1
+
 # Write Metadata to json
 with open("./_assets/metadata.json", "w") as of:
 	json.dump(traits, of, indent=2)
@@ -108,75 +431,57 @@ for item in traits:
 	if item["Background"] != "None":
 		img1 = Image.open(f'./_assets/bg/{bg_files[item["Background"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img1 = ""
+		img1 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img1 = ""
 
 	if item["Skin"] != "None":
 		img2 = Image.open(f'./_assets/sk/{sk_files[item["Skin"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img2 = ""
+		img2 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img2 = ""
 
 	if item["Mouth"] != "None":
 		img3 = Image.open(f'./_assets/mo/{mo_files[item["Mouth"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img3 = ""
+		img3 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img3 = ""
 
 	if item["Eyes"] != "None":
 		img4 = Image.open(f'./_assets/ey/{ey_files[item["Eyes"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img4 = ""
+		img4 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img4 = ""
 
 	if item["Crown"] != "None":
 		img5 = Image.open(f'./_assets/cr/{cr_files[item["Crown"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img5 = ""
+		img5 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img5 = ""
 
 	if item["Footwear"] != "None":
 		img6 = Image.open(f'./_assets/fw/{fw_files[item["Footwear"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img6 = ""
+		img6 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img6 = ""
 
 	if item["Accessories"] != "None":
 		img7 = Image.open(f'./_assets/ac/{ac_files[item["Accessories"]]}.png').convert('RGBA')
 	else:
-		# : Image.open(f'./_assets/none.png').convert('RGBA')
-		img7 = ""
+		img7 = Image.open(f'./_assets/none.png').convert('RGBA')
+		# img7 = ""
 
 	# Mash images
-	if img2 == "":
-		com1 = img1
-	else:
-		com1 = Image.alpha_composite(img1, img2)
+	com1 = Image.alpha_composite(img1, img2)
 
-	if img3 == "":
-		com2 = com1
-	else:
-		com2 = Image.alpha_composite(com1, img3)
+	com2 = Image.alpha_composite(com1, img3)
 
-	if img4 == "":
-		com3 = com2
-	else:
-		com3 = Image.alpha_composite(com2, img4)
+	com3 = Image.alpha_composite(com2, img4)
 
-	if img5 == "":
-		com4 = com3
-	else:
-		com4 = Image.alpha_composite(com3, img5)
+	com4 = Image.alpha_composite(com3, img5)
 
-	if img6 == "":
-		com5 = com4
-	else:
-		com5 = Image.alpha_composite(com4, img6)
+	com5 = Image.alpha_composite(com4, img6)
 
-	if img7 == "":
-		com6 = com5
-	else:
-		com6 = Image.alpha_composite(com5, img7)
+	com6 = Image.alpha_composite(com5, img7)
 
 
 	# Convert to RGB
