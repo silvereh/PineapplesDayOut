592789a923c2b2623e86dd65a139dce6596826e0
Silvere <silvere.heraudeau@gmail.com>
2021-07-26 18:43:44 -0600

    Tried using CORS library

--- docs/web3/pineapples.js
+++ docs/web3/pineapples.js
@@ -1,4 +1,4 @@
-import { ethers } from "https://cdn.ethers.io/lib/ethers-5.1.esm.min.js";
+// import { ethers } from "https://cdn.ethers.io/lib/ethers-5.1.esm.min.js";
 
 window.addEventListener('DOMContentLoaded', () => {
 	const MAX_SUPPLY = 5000;
